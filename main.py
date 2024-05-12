c0d3f6ec02dba11fe04b5cef4e618282264f591f
Raimkaa <htfyrik@gmail.com>
2024-05-12 16:31:51 +0500

    add files of the project

---
+++ main.py
@@ -0,0 +1,10 @@
+from config import db_config
+
+import psycopg2
+connection = psycopg2.connect(**db_config)
+cursor = connection.cursor()
+
+students = cursor.execute('SELECT * FROM students')
+students = cursor.fetchall()
+print(students)
+
